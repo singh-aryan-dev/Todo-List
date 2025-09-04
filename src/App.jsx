f443cc0beb1e149d9ad11cceeb9cbac6b66ff9f1
singh-aryan-dev <aryandineshsingh1@gmail.com>
2025-09-04 13:17:35 +0530

    file added

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import "./style.css";
+import { NewTodoForm } from "./NewTodoForm";
+import TodoList from "./TodoList";
+
+function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue === null) return [];
+    return JSON.parse(localValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo(title) {
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        {
+          id: crypto.randomUUID(),
+          title,
+          completed: false,
+        },
+      ];
+    });
+    
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      console.log("currentTodos", currentTodos);
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  console.log("himanshu", todos);
+  return (
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="Header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
+  );
+}
+
+export default App;
